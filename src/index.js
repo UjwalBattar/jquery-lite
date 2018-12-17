21acf3c555d612bba7f754b314a8450dcaf2889c
Ujwal Battar <ujwal.battar@gmail.com>
2018-12-16 17:35:38 -0800

    extend functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const DOMNodeCollection = require('./dom_node_collection');
 
-const $l = function (args) {
+const _docReadyCallbacks = [];
+let _docReady = false;
+
+window.$l = function (args) {
     switch (typeof args) {
+        case "function":
+            return registerDocReadyCallback(args);
         case "string":
-            return Array.prototype.slice.call(document.querySelectorAll(args));
+            return getNodesFromDOM(args);
         case "object":
             if (args instanceof HTMLElement) {
                 return new DOMNodeCollection([args]);
@@ -11,4 +16,33 @@ const $l = function (args) {
     }
 };
 
-window.$l = $l;
+$l.extend = function (baseObj, ...otherObjs) {
+    otherObjs.forEach((obj) => {
+        Object.assign(baseObj, obj);
+    });
+    return baseObj;
+};
+
+$l.ajax = function (options) {
+    console.log('More to come!')
+};
+
+getNodesFromDOM = (selector) => {
+    let nodes = document.querySelectorAll(selector);
+    // let nodesArray = Array.prototype.slice(nodes);
+    let nodesArray = Array.from(nodes);
+    return new DOMNodeCollection(nodesArray);
+};
+
+registerDocReadyCallback = (func) => {
+    if (!_docReady) {
+        _docReadyCallbacks.push(func);
+    } else {
+        func();
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    _docReady = true;
+    _docReadyCallbacks.forEach(func => func());
+});
